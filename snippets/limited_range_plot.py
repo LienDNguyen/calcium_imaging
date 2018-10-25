5150c7a56546db5ba7fa3ec4c33a2462957e8ba5
Daniel Schuette <d.schuette@online.de>
2018-10-25 20:36:56 +0200

    add limited_range_plot.py to ./snippets

---
+++ snippets/limited_range_plot.py
@@ -0,0 +1,44 @@
+#!/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
+# this snippets takes about 2 mins to complete
+# since the data file is pretty large (too many rows)
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+def plot_with_range(df, x, y, hue, limit, title):
+    """
+    this function plots a data frame within
+    a certain range of x values
+    ----------------------
+    arguments:
+        df    = data frame that holds the data to plot
+        x     = `df' column to plot on the x axis
+        y     = `df' column to plot on the y axis
+        hue   = `df' column to use for grouping
+        limit = tuple that determines the min
+                and max values of the x axis
+        title = title of the plot
+    """
+    sns.lineplot(x=x, y=y, hue=hue, data=df)
+    plt.xlim(limit[0], limit[1])
+    plt.title(title)
+    plt.show("hold")  # only necessary if run from cmd prompt
+
+
+# import data from ./assets folder
+print("importing data...")
+df = pd.read_excel("../assets/CTRL_E120Q.xlsx")
+
+# use the newly defined function for plotting the data
+print("plotting data...")
+plot_with_range(
+    df=df,
+    x="Time (s)",
+    y="F/Fo",
+    hue="Genotype",
+    limit=(0, 200),
+    title="Plot with Limited x Axis")
+
+# save the plot to the ./assets folder
+plt.savefig("limited_range_plot.png")
